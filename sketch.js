9b410972484c1228080fee5ea59a5dba81127d96
gabrielferreiradamazio <145364624+gabrielferreiradamazio@users.noreply.github.com>
2023-09-25 11:58:19 -0300

    carregamento arquivos html, css e js

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(800, 800);
+background("black")
+}
+f
+function draw() {
+  
+  
+  stroke("yellow");
+  fill("black");
+ 
+
+ if (mouseIsPressed) {
+  rect(mouseX, mouseY, 20, 35);
+ } 
+
+}
